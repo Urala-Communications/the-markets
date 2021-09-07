22f4a75165dde32771786f5503e3fc1e4c4d39e3
Alan Sutherland <sutherland_a@me.com>
2021-09-07 15:30:20 +0900

    Gitignore s

---
+++ pages/personal-finance/_slug.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="article-container">
+    <div v-if="post">
+      <!-- <Comparison v-if="post.comparison" :post="post" /> -->
+      <div class="article bg-white">
+      <!-- <b-jumbotron class="text-center" text-variant="white" :style="{backgroundImage:`url(${require('(./../assets/'+slug+'.jpg')})`}">
+        <p>Personal Finance</p>
+        <h1>{{ article }}</h1>
+        <p>By Rebecca Hey</p>
+        <span class="credit">© GETTY IMAGES / VANNI BASSETT</span>
+      </b-jumbotron> -->
+        <div class="container content buffer pb-5">
+          <div class="row d-flex justify-content-center">
+            <div class="col-12 py-3">
+              <h1 class="title" v-text="post.title" />
+              <div class="content">
+                <SanityContent
+                  v-for="child in post.body"
+                  :blocks="[child]"
+                  v-bind:key="child._id"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Comparison from './../../components/Comparison.vue';
+// import { SanityContent } from '@nuxtjs/sanity/dist/components/sanity-content'
+// import { groq } from '@nuxtjs/sanity'
+// export default {
+//   // components: { SanityContent, Comparison },
+//   async asyncData({ params, $sanity }) {
+//     const query = groq`*[_type == "post" && slug.current == "${params.slug}"][0]`
+//     const post = await $sanity.fetch(query)
+//     // console.log(post)
+//     return { post }
+//   }
+// }
+</script>
+
+<style lang="scss">
+
+.article-container {
+  margin-top: -1rem;
+  .container {
+    min-height: 100vh;
+  }
+
+  .content {
+    margin: 0 auto;
+    /* max-width: 38rem; */
+  }
+
+  p { margin: 1rem 0; }
+}
+
+.jumbotron {
+    margin-top: -1rem;
+    margin-bottom: 0;
+    max-height: 810px;
+    height: 74vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    border-radius: 0;
+    @include title-font();
+    .credit{
+      position: absolute;
+      bottom: 5px;
+      left: 5px;
+      font-size: 12px;
+    }
+  }
+  .article {
+    h2 {
+      font-size: 28px;
+      margin-bottom: 0.5rem;
+    }
+    h3{
+      font-size: 22px;
+    }
+    .container > .row {
+      max-width: 1040px;
+      margin: 0 auto;
+    }
+    .social {
+      max-width: 120px;
+      margin-bottom: 1.5rem;
+      .social-icon img {
+        width: 24px;
+      }
+    }
+    button{
+      width: 280px;
+    }
+  }
+  p {
+      margin-bottom: 0.3rem;
+  }
+  hr {
+      border-top: 1px solid rgb(0 0 0 / 100%);
+      display: block;
+      width: 100%;
+      margin: 0.5rem 0;
+  }
+  em {
+    font-size: 18px;
+    color: #90a4be;
+  }
+  .content-anchor {
+    display: block;
+    position: relative;
+    top: -110px;
+    visibility: hidden;
+  }
+  .card {
+    max-width: 500px;
+  }
+</style>
---
+++ pages/personal-finance/article.vue
@@ -0,0 +1,327 @@
+<template>
+  <div class="article bg-white">
+    <b-jumbotron class="text-center" text-variant="white" :style="{backgroundImage:`url(${require('(./../../assets/'+slug+'.jpg')})`}">
+      <p>Personal Finance</p>
+      <h1>{{ article }}</h1>
+      <p>By Rebecca Hey</p>
+      <span class="credit">© GETTY IMAGES / VANNI BASSETT</span>
+    </b-jumbotron>
+    <div class="container content buffer pb-5">
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 py-3">
+          <em class="mb-3 d-block">We publish unbiased reviews; our opinions are our own and are not influenced by payments from advertisers. Learn about our independent review process and partners in our advertiser disclosure.</em>
+          <strong>By Rebecca Hey</strong>
+          <p>Published: January 23, 2021	Updated: March 2, 2021</p>
+          <hr>
+          <div class="social d-flex justify-content-between">
+            <a href="" class="social-icon">
+              <img src="./../../assets/twitter-black.svg" alt="">
+            </a>
+            <a href="" class="social-icon">
+              <img src="./../../assets/fb-black.svg" alt="">
+            </a>
+            <a href="" class="social-icon">
+              <img src="./../../assets/youtube-black.svg" alt="">
+            </a>
+            <a href="" class="social-icon">
+              <img src="./../../assets/instagram-black.svg" alt="">
+            </a>
+          </div>
+          <p>At The Markets, we name the best credit cards based on the features we think are the most valuable for different sets of needs. Whether you’re looking for travel rewards, cash back on everyday purchases, a way to build credit, or a better way to pay down costly debt, we’ve done the research and crunched numbers to find the very best option for you. These are the best credit cards you can get right now, and we highly recommend giving each offer a second look:</p>
+        </div>
+        <div class="col-12 py-3">
+          <h4>Table of Contents</h4>
+          <ul>
+            <li>
+              <a href="#first">HSBC Cash Rewards Mastercard®</a>
+            </li>
+            <li>
+              <a href="#second">Business Advantage Travel Rewards Credit Card</a>
+            </li>
+            <li>
+              <a href="#third">Wells Fargo Propel American Express® Card</a>
+            </li>
+            <li>
+              <a href="#fourth">Discover It® Chrome For Students</a>
+            </li>
+            <li>
+              <a href="#fifth">Chase Sapphire Reserve®</a>
+            </li>
+            <li>
+              <a href="#sixth">Discover It® Secured</a>
+            </li>
+            <li>
+              <a href="#seventh">The Platinum Card® From American Express</a>
+            </li>
+            <li>
+              <a href="#eigth">Wells Fargo Platinum Card</a>
+            </li>
+            <li>
+              <a href="#ninth">American Express Gold Card</a>
+            </li>
+            <li>
+              <a href="#tenth">MBNA Platinum Card</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 py-3">
+          <span id="first" class="content-anchor"></span>
+          <h2>Best Overall</h2>
+          <h3>HSBC Cash Rewards Mastercard®</h3>
+          <div class="d-flex">
+            <div>
+              <img class="card" src="./../../assets/card.jpg" alt="hsbc">
+            </div>
+            <div class="d-flex flex-column px-4">
+              <div class="rating four">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <span></span>
+              </div>
+              <p>Earn an Introductory 3% Cash Back on all purchases in the first 12 months from Account opening, up to the first $10,000 in purchases. 1.5% cash back on all other purchases.</p>
+              <b-button class="mt-4" variant="dark">Sign up now</b-button>
+            </div>
+          </div>
+          <div>
+            <h4>Why we chose this card</h4>
+            <p>We think this card is a worthy addition to anyone’s wallet. The HSBC Cash Rewards card touts a great rewards scheme for a simple cash-back card: You’ll get double cash-back (3%, instead of the normal but competitive 1.5%) on every purchase for one year, up to $10,000 spent on purchases. After that, you’ll earn an unlimited 1.5% back on everything, so there are no bonus categories or additional earning limits to worry about. There’s a nice 12-month-long 0% APR offer that could help you finance a large purchase or two, and like all good cash-back rewards cards, you won’t pay an annual fee.</p>
+            <h4 class="mt-3">What do we like about it?</h4>
+            <ul>
+              <li>Earn 2% cashback on purchases</li>
+              <li>Unlimited cashback</li>
+              <li>No annual fees</li>
+            </ul>
+            <h4>Features</h4>
+            <ul>
+              <li>Representative APR 13.99% to 23.99% (variable)</li>
+              <li>Purchase rate from 13.99%</li>
+              <li>Credit limit based on credit score</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 py-3">
+          <span id="second" class="content-anchor"></span>
+          <h2>Best Overall</h2>
+          <h3>HSBC Cash Rewards Mastercard®</h3>
+          <div class="d-flex">
+            <div>
+              <img class="card" src="./../../assets/card.jpg" alt="hsbc">
+            </div>
+            <div class="d-flex flex-column px-4">
+              <div class="rating four">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <span></span>
+              </div>
+              <p>Earn an Introductory 3% Cash Back on all purchases in the first 12 months from Account opening, up to the first $10,000 in purchases. 1.5% cash back on all other purchases.</p>
+              <b-button class="mt-4" variant="dark">Sign up now</b-button>
+            </div>
+          </div>
+          <div>
+            <h4>Why we chose this card</h4>
+            <p>We think this card is a worthy addition to anyone’s wallet. The HSBC Cash Rewards card touts a great rewards scheme for a simple cash-back card: You’ll get double cash-back (3%, instead of the normal but competitive 1.5%) on every purchase for one year, up to $10,000 spent on purchases. After that, you’ll earn an unlimited 1.5% back on everything, so there are no bonus categories or additional earning limits to worry about. There’s a nice 12-month-long 0% APR offer that could help you finance a large purchase or two, and like all good cash-back rewards cards, you won’t pay an annual fee.</p>
+            <h4 class="mt-3">What do we like about it?</h4>
+            <ul>
+              <li>Earn 2% cashback on purchases</li>
+              <li>Unlimited cashback</li>
+              <li>No annual fees</li>
+            </ul>
+            <h4>Features</h4>
+            <ul>
+              <li>Representative APR 13.99% to 23.99% (variable)</li>
+              <li>Purchase rate from 13.99%</li>
+              <li>Credit limit based on credit score</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 py-3">
+          <span id="third" class="content-anchor"></span>
+          <h2>Best Overall</h2>
+          <h3>HSBC Cash Rewards Mastercard®</h3>
+          <div class="d-flex">
+            <div>
+              <img class="card" src="./../../assets/card.jpg" alt="hsbc">
+            </div>
+            <div class="d-flex flex-column px-4">
+              <div class="rating four">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <span></span>
+              </div>
+              <p>Earn an Introductory 3% Cash Back on all purchases in the first 12 months from Account opening, up to the first $10,000 in purchases. 1.5% cash back on all other purchases.</p>
+              <b-button class="mt-4" variant="dark">Sign up now</b-button>
+            </div>
+          </div>
+          <div>
+            <h4>Why we chose this card</h4>
+            <p>We think this card is a worthy addition to anyone’s wallet. The HSBC Cash Rewards card touts a great rewards scheme for a simple cash-back card: You’ll get double cash-back (3%, instead of the normal but competitive 1.5%) on every purchase for one year, up to $10,000 spent on purchases. After that, you’ll earn an unlimited 1.5% back on everything, so there are no bonus categories or additional earning limits to worry about. There’s a nice 12-month-long 0% APR offer that could help you finance a large purchase or two, and like all good cash-back rewards cards, you won’t pay an annual fee.</p>
+            <h4 class="mt-3">What do we like about it?</h4>
+            <ul>
+              <li>Earn 2% cashback on purchases</li>
+              <li>Unlimited cashback</li>
+              <li>No annual fees</li>
+            </ul>
+            <h4>Features</h4>
+            <ul>
+              <li>Representative APR 13.99% to 23.99% (variable)</li>
+              <li>Purchase rate from 13.99%</li>
+              <li>Credit limit based on credit score</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: 'The Markets - ' + this.article
+      }
+    },
+    async asyncData({ params }) {
+      const slug = params.article;
+      const article = slug.replace(/-/g, ' ').split(' ').map(w => w[0].toUpperCase()+ w.substring(1)).join(' ');
+      return { slug, article }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .jumbotron {
+    margin-top: -1rem;
+    margin-bottom: 0;
+    max-height: 810px;
+    height: 74vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    border-radius: 0;
+    @include title-font();
+    .credit{
+      position: absolute;
+      bottom: 5px;
+      left: 5px;
+      font-size: 12px;
+    }
+  }
+  .article {
+    h2 {
+      font-size: 28px;
+      margin-bottom: 0.5rem;
+    }
+    h3{
+      font-size: 22px;
+    }
+    .container > .row {
+      max-width: 1040px;
+      margin: 0 auto;
+    }
+    .social {
+      max-width: 120px;
+      margin-bottom: 1.5rem;
+      .social-icon img {
+        width: 24px;
+      }
+    }
+    button{
+      width: 280px;
+    }
+  }
+  p {
+      margin-bottom: 0.3rem;
+  }
+  hr {
+      border-top: 1px solid rgb(0 0 0 / 100%);
+      display: block;
+      width: 100%;
+      margin: 0.5rem 0;
+  }
+  em {
+    font-size: 18px;
+    color: #90a4be;
+  }
+  .content-anchor {
+    display: block;
+    position: relative;
+    top: -110px;
+    visibility: hidden;
+  }
+  .card {
+    max-width: 500px;
+  }
+  .rating {
+    display: flex;
+    align-items: center;
+    margin-bottom: 16px;
+    .star{
+      height: 32px;
+      width: 32px;
+      margin-right: 7px;
+      background-image: url('./../../assets/empty-star.svg');
+      background-repeat: no-repeat;
+    }
+    span:before{color: #90a4be; font-size: 12px;}
+    &.one{
+      .star:nth-of-type(1) {
+        background-image: url('./../../assets/star.svg');
+      }
+      span:before {
+        content: '1.0';
+      }
+    }
+    &.two{
+      .star:nth-of-type(-n+2) {
+        background-image: url('./../../assets/star.svg');
+      }
+      span:before {
+        content: '2.0';
+      }
+    }
+    &.three{
+      .star:nth-of-type(-n+3) {
+        background-image: url('./../../assets/star.svg');
+      }
+      span:before {
+        content: '3.0';
+      }
+    }
+    &.four{
+      .star:nth-of-type(-n+4) {
+        background-image: url('./../../assets/star.svg');
+      }
+      span:before {
+        content: '4.0';
+      }
+    }
+    &.five{
+      .star {
+        background-image: url('./../../assets/star.svg');
+      }
+      span:before {
+        content: '5.0';
+      }
+    }
+  }
+  @media(max-width: 768px){
+    .jumbotron{
+      height: 44vh;
+    }
+  }
+</style>
