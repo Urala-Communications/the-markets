9f8d3f0fcac3abd876de92756f78b0e63086bda4
Alan Sutherland <sutherland_a@me.com>
2021-12-23 10:46:23 +0000

    Backend strap dir

---
+++ backend/config/env/production/server.js
@@ -0,0 +1,3 @@
+module.exports = ({ env }) => ({
+  url: env('MARKETS_HEROKU_URL'),
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '768d70d2-e187-45fb-98c1-3b293610df23'
+};
